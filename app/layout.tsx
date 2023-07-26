072396b31625c72895989cba4c7dd80e1df7f40d
cosmicwhiz <cosmicwhiz@gmail.com>
2023-07-26 22:35:27 +0530

    implemented next fonts

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,7 +2,32 @@ import '../styles/globals.css'
 import type { Metadata } from 'next'
 import Nav from '@/components/Nav'
 import Provider from '@/components/Provider'
+import { Inter, Poppins, Montserrat, Orbitron } from 'next/font/google'
 
+
+const inter = Inter({
+  subsets: ["latin"],
+  weight: ["400", "500"],
+  variable: "--font-inter",
+});
+
+const montserrat = Montserrat({
+  subsets: ["latin"],
+  weight: "400",
+  variable: "--font-montserrat",
+});
+
+const orbitron = Orbitron({
+  subsets: ["latin"],
+  weight: "400",
+  variable: "--font-orbitron",
+});
+
+const poppins = Poppins({
+  subsets: ["latin"],
+  weight: "400",
+  variable: "--font-poppins",
+});
 
 export const metadata: Metadata = {
   title: 'AuthX',
@@ -15,7 +40,7 @@ export default function RootLayout({
   children: React.ReactNode
 }) {
   return (
-    <html lang="en">
+    <html lang="en" className={`${inter.variable} ${orbitron.variable} ${poppins.variable} ${montserrat.variable}`}>
       <link rel='icon' href='/favicon.ico'/>
       <body>
         <Provider>
